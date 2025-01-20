4197898728bb38ec41e63c4fe154341357a2b80f
fian14-id <allifian051@gmail.com>
2025-01-20 12:17:46 +0700

    add manga popular page

---
+++ src/app/popular/manga/page.jsx
@@ -0,0 +1,14 @@
+import Banner from "@/components/utilities/Banner";
+import Pagination from "@/components/utilities/Pagination";
+
+const Page = () => {
+    return (
+        <section>
+            <Banner />
+            <h1>Ini Popular</h1>
+            <Pagination />
+        </section>
+    )
+}
+
+export default Page
