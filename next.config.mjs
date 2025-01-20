19ed00d2dc1201d72316b7284532a1756441f1ef
fian14-id <allifian051@gmail.com>
2025-01-20 11:53:33 +0700

    remove config experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['api.jikan.moe'],
         remotePatterns: [
             {
                 hostname: "cdn.myanimelist.net",
             }
         ]
     },
-    experimental: {
-        serverActions: true,
-    }
 };
 
 export default nextConfig;
